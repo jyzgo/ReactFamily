bf7fbd86f1d9ff24a76da8d2e968562bb0743a8b
jyzHome <jyzgo@163.com>
2018-06-05 10:08:55 +1000

    first webpack

---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("her");
+const path = require('path');
+//document.getElementById('app').innerHTML ="Webpack works"
+console.log("df " + path.join(__dirname,'src/index.js'));
+console.log("path " + path.join(__dirname,'/dist'));
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
-module.exports = {
-        entry:path.join(__dirname,'src/index.js'),
 
-        output:{
-                path:path.join(__dirname,'./dist'),
-                filename:'bundle.js'
-        }
+module.exports = {
+
+    /*入口*/
+    entry: path.join(__dirname, 'src/index.js'),
+
+    /*输出到dist文件夹，输出文件名字为bundle.js*/
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'bundle.js'
+    }
 };
